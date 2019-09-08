3a75a5e4051db013b5da849dc2bec5eb4e3b09e1
Nehal Udyavar <nehaludyavar@gmail.com>
2019-09-08 00:40:43 -0700

    Data fetching hook in JSON tree viewer component

--- client/src/containers/CustomTabContent.js
+++ client/src/containers/CustomTabContent.js
@@ -11,41 +11,64 @@ import FetchTreeViewer from "./FetchTreeViewer";
 // Styling
 import styles from "./CustomTabContent.module.css";
 
-const CustomTabContent = ({ model }) => (
-  <div className={styles.customTabContent}>
-    <TabContainer id="models-left-tab-nav" defaultActiveKey="create">
-      <Row style={{ minHeight: "500px", marginRight: "0px" }}>
-        <Col sm={3}>
-          <Nav variant="pills" className="flex-column">
-            <Nav.Item>
-              <Nav.Link eventKey="create">Create</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="fetch">Fetch</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="update">Update</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="delete">Delete</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Col>
-        <Col sm={9} className={styles.tabPane}>
-          <TabContent>
-            <TabPane eventKey="create">
-              <CreateForms model={model} />
-            </TabPane>
-            <TabPane eventKey="fetch">
-              <FetchTreeViewer />
-            </TabPane>
-            <TabPane eventKey="update">Update...</TabPane>
-            <TabPane eventKey="delete">Delete...</TabPane>
-          </TabContent>
-        </Col>
-      </Row>
-    </TabContainer>
-  </div>
-);
+const CustomTabContent = ({ model }) => {
+  const selectDataFetchingUrl = model => {
+    switch (model) {
+      case "source":
+        return "/api/sources";
+      default:
+        return null;
+    }
+  };
+
+  const selectActionType = model => {
+    switch (model) {
+      case "source":
+        return "FETCH_SOURCES";
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className={styles.customTabContent}>
+      <TabContainer id="models-left-tab-nav" defaultActiveKey="create">
+        <Row style={{ minHeight: "500px", marginRight: "0px" }}>
+          <Col sm={3}>
+            <Nav variant="pills" className="flex-column">
+              <Nav.Item>
+                <Nav.Link eventKey="create">Create</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="fetch">Fetch</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="update">Update</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="delete">Delete</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={9} className={styles.tabPane}>
+            <TabContent>
+              <TabPane eventKey="create">
+                <CreateForms model={model} />
+              </TabPane>
+              <TabPane eventKey="fetch">
+                <FetchTreeViewer
+                  dataFetchUrl={selectDataFetchingUrl(model)}
+                  actionType={selectActionType(model)}
+                />
+              </TabPane>
+              <TabPane eventKey="update">Update...</TabPane>
+              <TabPane eventKey="delete">Delete...</TabPane>
+            </TabContent>
+          </Col>
+        </Row>
+      </TabContainer>
+    </div>
+  );
+};
 
 export default CustomTabContent;
--- client/src/containers/FetchTreeViewer/index.js
+++ client/src/containers/FetchTreeViewer/index.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
 import JSONTree from "react-json-tree";
 import theme from "./flatTheme";
 
-const data = {
-  name: "Youtube",
-  url: "www.youtube.com"
-};
+const FetchTreeViewer = ({ model, dataFetchUrl, actionType }) => {
+  const source = useSelector(state => state.source);
+  const dispatch = useDispatch();
 
-const FetchTreeViewer = ({ model }) => {
-  return <JSONTree data={data} theme={theme} invertTheme={true} />;
+  useEffect(() => {
+    const fetchData = async () => {
+      if (dataFetchUrl) {
+        const res = await axios.get(dataFetchUrl);
+        dispatch({ type: actionType, payload: res.data });
+      }
+    };
+    fetchData();
+  }, [dispatch, dataFetchUrl, actionType]);
+
+  return <JSONTree data={source} theme={theme} invertTheme={true} />;
 };
 
 export default FetchTreeViewer;
